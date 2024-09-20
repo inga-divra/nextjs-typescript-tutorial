2c834b6358c0372a6e2d521b357957ac365aae8d
inga-divra <ingady@gmail.com>
2024-09-20 11:49:56 +0300

    Learned to set up page structures, routes, and navigation using Link in Next.js.

---
+++ app/info/page.tsx
@@ -0,0 +1,5 @@
+const InfoPage = () => {
+  return <h1 className='text-7xl'>Info Page</h1>;
+};
+
+export default InfoPage;
--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,17 @@
+import Link from 'next/link';
+
 const HomePage = () => {
-  return <h1 className='text-7xl'>Home Page</h1>;
+  return (
+    <div>
+      <h1 className='text-7xl'>Home Page</h1>
+      <Link
+        href='/about'
+        className='text-xl text-blue-500 inline-block mt-8 ml-2'
+      >
+        About page
+      </Link>
+    </div>
+  );
 };
 
 export default HomePage;
