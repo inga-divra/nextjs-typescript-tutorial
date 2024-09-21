fd15f87a2935ae088342609adc32e3558c57cace
inga-divra <ingady@gmail.com>
2024-09-21 19:13:51 +0300

    I learned that Next.js has a Metadata API that allows defining static metadata like title, description, and keywords in layout.tsx or page.tsx to improve SEO and web shareability
    Also, learned about the difference between Client and Server Components in Next.js, their use cases, and how to implement interactive UI with client components using use client directive.

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,17 @@
 import Navbar from '@/components/Navbar';
+import type { Metadata } from 'next';
 import './globals.css';
 import { Inter, Inconsolata, Roboto } from 'next/font/google';
 
 const inter = Inter({ subsets: ['latin'] });
 //const inconsolata = Inconsolata({ subsets: ['latin'] });
 //const roboto = Roboto({ subsets: ['latin'], weight: ['400'] });
+
+export const metadata: Metadata = {
+  title: 'Next.js Project',
+  description: 'A Next.js project with TypeScript and TailwindCSS.',
+  keywords: 'Next.js, Typescript, TailwindCSS',
+};
 
 export default function RootLayout({
   children,
