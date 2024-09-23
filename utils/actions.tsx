4479dbe43a0fd61b1e90ca600dd6f993516b4797
inga-divra <ingady@gmail.com>
2024-09-23 14:55:27 +0300

    Learned how to implement user deletion in Next.js
    ... including creating a DeleteButton component, managing deletion actions on the server side with deleteUser, and optimizing form handling with the .bind option for passing dynamic values.

--- utils/actions.tsx
+++ utils/actions.tsx
@@ -30,7 +30,6 @@ export const createUser = async (prevState: any, formData: FormData) => {
     console.log(error);
     return 'failed to create a user...';
   }
-
   //redirect('/');
 };
 
@@ -44,4 +43,21 @@ const saveUser = async (user: User) => {
   const users = await fetchUsers();
   users.push(user);
   await writeFile('users.json', JSON.stringify(users));
+};
+
+export const deleteUser = async (formData: FormData) => {
+  const id = formData.get('id') as string;
+  const users = await fetchUsers();
+  const updatedUsers = users.filter((user: User) => user.id !== id);
+  await writeFile('users.json', JSON.stringify(updatedUsers));
+  revalidatePath('/actions');
+};
+export const removeUser = async (id: string, formData: FormData) => {
+  const name = formData.get('name') as string;
+  console.log(name);
+
+  const users = await fetchUsers();
+  const updatedUsers = users.filter((user: User) => user.id !== id);
+  await writeFile('users.json', JSON.stringify(updatedUsers));
+  revalidatePath('/actions');
 };
