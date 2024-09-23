0b50280e86a7bfcc88f6d043272d391888c8099b
inga-divra <ingady@gmail.com>
2024-09-23 15:49:16 +0300

    Learned how to handle HTTP methods like GET and POST in Next.js Route Handlers using NextRequest and NextResponse for handling server-side logic and advanced routing features

---
+++ app/api/users/route.ts
@@ -0,0 +1,17 @@
+import { NextRequest, NextResponse } from 'next/server';
+import { fetchUsers, saveUser } from '@/utils/actions';
+
+export const GET = async (request: NextRequest) => {
+    console.log(request.url);
+    console.log(request.nextUrl.searchParams.get('id'));
+
+    const users = await fetchUsers();
+    return Response.json({ users })
+};
+
+export const POST = async (request: Request) => {
+    const user = await request.json();
+    const newUser = { ...user, id: Date.now().toString() };
+    await saveUser(newUser);
+    return Response.json({ msg: 'user created' });
+};
